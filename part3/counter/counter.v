ff16aa07f660751e9019704637f9be44afd53e65
matt venn <matt@mattvenn.net>
2019-11-26 17:08:15 +0100

    counter example

---
+++ part3/counter/counter.v
@@ -0,0 +1,19 @@
+module counter (input clk);
+    localparam MAX_COUNT = 16'b1000;
+
+    reg [15:0] counter;
+
+    always @(posedge clk)
+        if(counter == MAX_COUNT)
+            counter <= 0;
+        else
+            counter <= counter + 1'b1;
+
+`ifdef FORMAL
+    initial
+        assume(counter == 0);
+
+    always @(*)
+        assert(counter <= MAX_COUNT);
+`endif
+endmodule
